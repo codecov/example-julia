ff33f41c8b1cf915741ffc7de2591b77ced366ef
Tom Hu <tom@codecov.io>
2022-09-01 14:28:26 -0400

    fix: add in tests

--- src/calculator.jl
+++ src/calculator.jl
@@ -3,8 +3,7 @@ module calculator
   export add, subtract, mulitply, divide
   add(x, y) = x + y
   subtract(x, y) = x - y
-  mulitply(x, y) = x * y
+  multiply(x, y) = x * y
   divide(x, y) = y == 0  ? "Cannot divide by 0" : x * 1.0 / y
 
-  print("meow")
 end
